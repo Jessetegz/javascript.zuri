98c9ef70d7a934d48e0aa395149f43b5bae3fbb1
Votu-Sakpere Jesse <jessetegz@gmail.com>
2022-06-25 16:35:20 +0100

    javascript intro

---
+++ script.js
@@ -0,0 +1,7 @@
+document.write('My name is Votu-Sakpere Jesse')
+document.write('<br>')
+document.write('<br>')
+document.write('I am 180cm tall')
+document.write('<br>')
+document.write('<br>')
+document.write('I am a Nigerian')
